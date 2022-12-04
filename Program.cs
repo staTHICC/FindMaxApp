64a6e1eaee6bfa4036fbadef1cd30877ecfac0ff
StasCool <60066403+staTHICC@users.noreply.github.com>
2022-12-04 17:10:00 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b > a)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine("a");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
